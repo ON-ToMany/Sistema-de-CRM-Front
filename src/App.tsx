645d5fb1fed90dd00b5148d3250c9ad864188826
BrenoNunes96 <brenocp3@live.com>
2026-04-22 19:32:44 -0300

    cadastro oportunidade feita

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,26 @@ import Quemsomos from "./pages/quemsomos/Quemsomos";
 import { AuthProvider } from "./contexts/AuthContext"; 
 import LoginUsuario from "./pages/Login/Login";
 import CadastrarUsuario from "./pages/cadastrar/CadastrarUsuario";
+import CardOportunidade from "./components/oportunidade/CardOportunidade";
+
+import { ToastContainer } from 'react-toastify';
 
 
 
 function App() {
   return (
-    <AuthProvider> 
+ 
+ <AuthProvider> 
+     <ToastContainer/>
       <BrowserRouter>
         <div className="flex flex-col min-h-screen">
           <Routes>  
            {/* <Route path="/" element={<Home />} /> */}
             <Route path="/Login" element={<LoginUsuario />}/>
-            <Route path="/Cadastro" element={<CadastrarUsuario/>}  />
-            
+            <Route path="/cadatrar" element={<CadastrarUsuario/>}  />
+               <Route path="/" element={<CardOportunidade/>} />
             <Route path="/Quem-Somos" element={<Quemsomos />} />
-
+             
           </Routes>
         </div>
       </BrowserRouter>
