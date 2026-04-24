c3e6f380d147ccf09c01d286e1c87252e0e3cf60
mayaramonteiro <imayagmb@gmail.com>
2026-04-24 11:29:13 -0300

    fix: corrigindo rotas, apagando sidebar

--- src/pages/gerenciarempresa/DashboardEmpresa.tsx
+++ src/pages/gerenciarempresa/DashboardEmpresa.tsx
@@ -1,10 +1,8 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import type { FC } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import { RiHome2Line, RiAddFill } from "react-icons/ri";
 import Tabela from './components/tabela/Tabela';
-import Logo from "../../assets/logo GreenTech.png";
 import iconeCO2 from "../../assets/simbolo CO2.png.png";
 import iconeRecicle from "../../assets/simbolo recicle.png";
 
@@ -14,52 +12,6 @@ const DashboardEmpresa: FC = () => {
 
 return (
     <div className="flex flex-row h-screen bg-[#E9ECEF] w-full overflow-hidden font-sans">
-      {/* //apagar esse menu interno  */}
-      <aside className="group w-20 hover:w-64 h-full bg-[#E4EBE4] border-r border-[#0D542B] flex flex-col p-6 shadow-sm rounded-tr-[50px] rounded-br-[50px] z-10 transition-all duration-500 ease-in-out overflow-hidden">
-        
-        <div className="flex flex-row items-center gap-4 mb-16 w-full h-18 px-2">
-          <img src={Logo} alt="Logo" className="h-14 w-14 object-contain shrink-0 
-          transition-transform duration-300 group-hover:scale-110" />
-          <div className="flex flex-col leading-tight opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-            <span className="text-xl font-bold text-[#1A3321]">Greentech</span>
-            <span className="text-xl font-medium text-[#0D542B]">CRM</span>
-          </div>
-        </div>
-
-        <nav className="flex-1 flex flex-col gap-8 text-[#0D542B] font-bold">
-          <button 
-            onClick={() => navigate('/home')} 
-            className="flex items-center gap-4 hover:opacity-70 transition-opacity text-lg min-w-50 text-left"
-          >
-            <RiHome2Line size={28} className="shrink-0" /> 
-            <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300">Inicio</span>
-          </button>
-          
-          <button 
-            onClick={() => navigate('/cadastrarproduto')} 
-            className="flex items-center gap-4 hover:opacity-70 transition-opacity text-lg min-w-50 text-left"
-          >
-            <RiAddFill size={28} className="shrink-0" /> 
-            <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300">Gerenciar</span>
-          </button>
-
-          <button 
-            onClick={() => navigate('/produtos')} 
-            className="flex items-center gap-4 hover:opacity-70 transition-opacity text-lg min-w-50 text-left"
-          >
-            <RiAddFill size={28} className="shrink-0" /> 
-            <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300">Listar Todos</span>
-          </button>
-        </nav>
-
-        <button
-          onClick={() => navigate('/login')}
-          className="w-12 group-hover:w-32 h-10 bg-[#0D542B] text-white rounded-full font-bold text-xs mt-auto self-center uppercase tracking-wider hover:bg-green-900 transition-all duration-300 flex items-center justify-center overflow-hidden"
-        >
-          <span className="group-hover:inline hidden">SAIR</span>
-          <span className="group-hover:hidden inline">X</span>
-        </button>
-      </aside>
 
       <main className="flex-1 h-full overflow-y-auto p-8 flex justify-center">
         <div className="w-full max-w-5xl flex flex-col gap-6 bg-[#CBD5CB] rounded-[40px] p-8 border border-[#B5BEB5] shadow-md self-start">
