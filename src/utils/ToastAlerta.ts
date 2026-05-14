9e224deb9b693345c7e6eea163556d457527ba83
Maria Eduarda <mariaeduardao.gms@gmail.com>
2026-05-14 20:32:43 -0300

    style: ajusta estilo do toastify

--- src/utils/ToastAlerta.ts
+++ src/utils/ToastAlerta.ts
@@ -1,44 +1,47 @@
-import { toast } from 'react-toastify';
+import { Bounce, toast } from 'react-toastify';
 
 export function ToastAlerta(mensagem: string, tipo: string) {
     switch (tipo) {
         case 'sucesso':
             toast.success(mensagem, {
                 position: "top-right",
-                autoClose: 2000,
+                autoClose: 5000,
                 hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: false,
-                draggable: false,
-                theme: "colored",
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
                 progress: undefined,
+                theme: "colored",
+                transition: Bounce,
             });
-            break;
+        break;
 
         case 'erro':
             toast.error(mensagem, {
                 position: "top-right",
-                autoClose: 2000,
+                autoClose: 5000,
                 hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: false,
-                draggable: false,
-                theme: "colored",
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
                 progress: undefined,
+                theme: "colored",
+                transition: Bounce,
             });
-            break;
+        break;
 
         default:
             toast.info(mensagem, {
                 position: "top-right",
-                autoClose: 2000,
+                autoClose: 5000,
                 hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: false,
-                draggable: false,
-                theme: "colored",
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
                 progress: undefined,
+                theme: "colored",
+                transition: Bounce,
             });
-            break;
+        break;
     }
 }
